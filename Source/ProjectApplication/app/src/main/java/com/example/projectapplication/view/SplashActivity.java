66a967d9900e461debec7d3ed41bd8399a8b55ea
Lê Nguyên <nguyenle1999py@gmail.com>
2019-11-17 23:01:34 +0700

    Finish Login, Register
    Add login register layout, and finish login by facebook

--- Source/ProjectApplication/app/src/main/java/com/example/projectapplication/view/SplashActivity.java
+++ Source/ProjectApplication/app/src/main/java/com/example/projectapplication/view/SplashActivity.java
@@ -7,19 +7,31 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.view.Window;
+import android.widget.ProgressBar;
 
 import com.example.projectapplication.R;
 import com.example.projectapplication.manager.Constants;
 import com.example.projectapplication.network.MyAPIClient;
 
+import java.sql.Time;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Timer timer;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        progressBar =(ProgressBar)findViewById(R.id.progressBar);
+
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String accessToken = sharedPref.getString(getString(R.string.saved_access_token), null);
         long time = sharedPref.getLong(getString(R.string.saved_access_token_time), (long)0);
@@ -27,20 +39,35 @@ public class SplashActivity extends AppCompatActivity {
         long expire = (new Date()).getTime()/1000 - time;
         MyAPIClient.getInstance().setAccessToken(accessToken);
 
+        timer = new Timer();
+
+
+
         if (TextUtils.isEmpty(accessToken) || expire > Constants.expire_token) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-            return;
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            },1000);
         }else{
-            Intent intent = new Intent(this, ListTours.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-            return;
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, ListTours.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            },1000);
         }
     }
 
